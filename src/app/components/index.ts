62812609189f6f0886801c30985a66fc70a0bcab
obergodmar <obergodmar@gmail.com>
2020-04-20 15:07:37 +0300

    Add Letter Portal

--- src/app/components/index.ts
+++ src/app/components/index.ts
@@ -3,3 +3,4 @@ export {LetterBodyInput} from './letter-body-input/letter-body-input';
 export {LetterButton} from './letter-button/letter-button';
 export {ThemeButtons} from './theme-buttons/theme-buttons';
 export {LetterTooltip} from './letter-tooltip/letter-tooltip';
+export {LetterPortal} from './letter-portal/letter-portal';
