ae0ad7854771310548a84bdd6f088d1d14e1ea44
obergodmar <obergodmar@gmail.com>
2020-04-20 14:24:33 +0300

    Update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -3,63 +3,70 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const BrotliPlugin = require('brotli-webpack-plugin');
 const AssetsPlugin = require('assets-webpack-plugin');
 
-module.exports = {
-    entry: './src',
-    output: {
-        path: path.join(__dirname, './dist/'),
-        filename: 'main.bundle.js'
-    },
-    optimization: {
-        splitChunks: {
-            cacheGroups: {
-                commons: {
-                    test: /[\\/]node_modules[\\/]/,
-                    name: 'vendors',
-                    chunks: 'all'
-                }
-            }
-        }
-    },
-    resolve: {
-        extensions: ['.ts', '.tsx', '.js']
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(ts|js)x?$/,
-                exclude: /node_modules/,
-                use: {
-                loader: 'babel-loader'
-                },
-            },
-            {
-                test: /\.scss$/,
-                use: [
-                    {loader: 'style-loader'},
-                    {loader: 'css-loader'},
-                    {loader: 'sass-loader'}
-                ]
-            },
-            {
-                test: /\.(png|woff|woff2|eot|ttf|svg)$/,
-                use: ['url-loader?limit=100000']
-            }
-        ]
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-        template: './src/index.html'
-        }),
-        new AssetsPlugin({
+module.exports = () => {
+    const prodPlugins = [];
+    if (process.env.NODE_ENV === 'Production') {
+        prodPlugins.push(new AssetsPlugin({
             prettyPrint: true,
             filename: 'assets.json',
             path: path.resolve(__dirname, 'dist')
-        }),
-        new BrotliPlugin({
+        }));
+        prodPlugins.push(new BrotliPlugin({
             asset: '[path].br[query]',
             test: /\.(js|css|html|svg)$/,
             threshold: 10240,
             minRatio: 0.8
-        })
-    ]
+        }))
+    }
+
+    return {
+        entry: './src',
+        output: {
+            path: path.join(__dirname, './dist/'),
+            filename: 'main.bundle.js'
+        },
+        optimization: {
+            splitChunks: {
+                cacheGroups: {
+                    commons: {
+                        test: /[\\/]node_modules[\\/]/,
+                        name: 'vendors',
+                        chunks: 'all'
+                    }
+                }
+            }
+        },
+        resolve: {
+            extensions: ['.ts', '.tsx', '.js']
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.(ts|js)x?$/,
+                    exclude: /node_modules/,
+                    use: {
+                        loader: 'babel-loader'
+                    },
+                },
+                {
+                    test: /\.scss$/,
+                    use: [
+                        {loader: 'style-loader'},
+                        {loader: 'css-loader'},
+                        {loader: 'sass-loader'}
+                    ]
+                },
+                {
+                    test: /\.(png|woff|woff2|eot|ttf|svg)$/,
+                    use: ['url-loader?limit=100000']
+                }
+            ]
+        },
+        plugins: [
+            new HtmlWebpackPlugin({
+                template: './src/index.html'
+            }),
+            ...prodPlugins
+        ]
+    }
 };
